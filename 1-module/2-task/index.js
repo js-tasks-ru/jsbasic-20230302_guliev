dbd1d6b0427cb686e1d056520ba871809407270f
guliev <guliev@internet.ru>
2023-03-09 11:23:30 +0200

    fix

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (name === null) return false;
+  if(name.indexOf(' ') >= 0) return false;
   return name.length > 3;
 }
 
@@ -22,3 +24,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+console.log(isValid('anar'));
