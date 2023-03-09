a1911729302c0208c01847f4400bd064b3228f25
guliev <guliev@internet.ru>
2023-03-09 15:07:45 +0200

    AllHomeworks

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name === null) return false;
+  if (name.indexOf(' ') >= 0) return false;
+  return name.length > 3;
 }
 
 function sayHello() {
