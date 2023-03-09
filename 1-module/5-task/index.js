9d524dd1240c3210b79fa4374697b279a4b25554
guliev <guliev@internet.ru>
2023-03-09 10:19:06 +0200

    fix

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,7 +1,11 @@
 function truncate(str, maxlength) {
-  if (str.length > maxlength) {
-    return `${str.substr(0, maxlength - 3)}...`;
+  if (typeof str === 'string') {
+    if (str?.length > maxlength) {
+      return `${str.substr(0, maxlength - 3)}...`;
+    } else {
+      return str;
+    }
   } else {
-    return str;
+    return false;
   }
 }
