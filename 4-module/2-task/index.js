f6a94a6dcb10301c6b39ad9a7e1c983e7a98114e
Anar Guliev <guliev@internet.ru>
2023-03-26 15:57:28 +0300

    Lession4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,33 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const tr = table.getElementsByTagName('tr');
+  let painted = 0;
+
+  Array.from(tr).forEach((item) => {
+    const td = item.getElementsByTagName('td');
+
+    for (let i = 0; i <= td.length; i++) {
+      if (i === painted) {
+        td[i].style.cssText = 'background-color: red;'
+        painted += 1;
+        break;
+      }
+    }
+  });
+
+  // Это можно не смотреть, оставляю тут для себя =)
+
+  // const tr = table.getElementsByTagName('tr');
+  // let painted = tr.length - 1;
+
+  // Array.from(tr).forEach((item) => {
+  //   const td = item.getElementsByTagName('td');
+
+  //   for (let i = 0; i <= td.length; i++) {
+  //     if (i === painted) {
+  //       td[i].style.cssText = 'background-color: red;'
+  //       painted = painted - 1;
+  //       break;
+  //     }
+  //   }
+  // });
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  const tr = table.getElementsByTagName('tr');
+
+  Array.from(tr).forEach((item) => {
+    const td = item.getElementsByTagName('td');
+
+    if (td[td.length - 1].getAttribute('data-available')) {
+      item.className = JSON.parse(td[td.length - 1].getAttribute('data-available')) ? 'available' : 'unavailable';
+    }
+
+    if (td[td.length - 2].innerHTML !== 'Gender') {
+      item.className += td[td.length - 2].innerHTML === 'm' ? ' male' : ' female';
+    }
+
+    if (td[td.length - 3].innerHTML < 18) {
+      item.style.cssText = 'text-decoration: line-through';
+    }
+
+    if (td[td.length - 1].innerHTML !== 'Status' && td[td.length - 1].getAttribute('data-available') === null) {
+      item.setAttribute('hidden', true);
+    }
+  });
 }
