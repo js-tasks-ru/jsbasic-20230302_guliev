62250566aaed7154094c81933d2fd7c21019b880
Anar Guliev <guliev@internet.ru>
2023-03-30 22:32:11 +0300

    5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,26 @@
 function initCarousel() {
-  // ваш код...
+  const rightBtn = document.querySelector('.carousel__arrow_right');
+  const leftBtn = document.querySelector('.carousel__arrow_left');
+  const carousel = document.querySelector('.carousel__inner');
+  let num = 0;
+
+  rightBtn.addEventListener('click', () => {
+    num += 1;
+    carousel.style.transform = `translateX(${-carousel.offsetWidth * num}px)`
+
+    if(num === 3){
+      rightBtn.style.display = 'none';
+      leftBtn.style.display = '';
+    } 
+  });
+
+  leftBtn.addEventListener('click', () => {
+    num = num - 1;
+    carousel.style.transform = `translateX(${-carousel.offsetWidth * num}px)`
+
+    if(num === 0){
+      rightBtn.style.display = '';
+      leftBtn.style.display = 'none';
+    } 
+  })
 }
