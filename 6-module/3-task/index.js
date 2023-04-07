d90bd91ec320d775a91a06a40d7ae7e16b3c1c63
Anar Guliev <guliev@internet.ru>
2023-04-07 09:09:10 +0300

    fix

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,62 +1,81 @@
 import createElement from '../../assets/lib/create-element.js';
 
-{/* <div class="carousel__slide" data-id="penang-shrimp">
-  <img src="/assets/images/carousel/...значение slide.image..." class="carousel__img" alt="slide">
-  <div class="carousel__caption">
-    <span class="carousel__price">€<!--значение slide.price--></span>
-    <div class="carousel__title"><!--значение slide.name--></div>
-    <button type="button" class="carousel__button">
-      <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-    </button>
-  </div>
-</div> */}
-
 export default class Carousel {
   elem;
 
   constructor(slides) {
-    this.slides = slides;
-    this.createSlides(slides);
-
-    return this.elem;
+    this.elem = createElement(this.#carousel(slides));
   }
 
-  createSlides(slides) {
-    slides.forEach((slide) => {
-      console.log(slide);
-      this.elem = createElement(`<div class='carousel__slide' data-id=${slide.id}></div>`);
-      this.putImg(this.createImg(slide.image));
-      this.putCarouselCaption(this.createCarouselCaption(`€${slide.price.toFixed(2)}`, slide.name));
-      console.log(this.elem);
-    })
+  #carousel(slides) {
+    const slide = slides.map((slide) => this.#carouselSlide(slide.image, slide.price, slide.name))
+
+    return (
+      `
+        <div class='carousel'>
+            ${this.#tempateArrows()}
+            <div class="carousel__inner">
+              ${slide.join(',').replaceAll(',')}
+            </div>
+        </div>
+      `
+    )
   }
 
-  createImg(url) {
-    const img = document.createElement('img');
-    img.src = `/assets/images/carousel/${url}`
-    img.alt = 'slide';
-    img.classList = 'carousel__img'
-    return img;
+  #tempateArrows(){
+    return (
+      `
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+      `
+    )
   }
 
-  putImg(img) {
-    this.elem.appendChild(img);
+  #carouselSlide(url, price, title) {
+    return (
+      `
+        <div class="carousel__slide" data-id="penang-shrimp">
+          ${this.#carouselImg(url)}
+          ${this.#carouselCaption(price, title)}
+        </div>
+      `
+    );
   }
 
-  createCarouselCaption(price, title) {
-    const carouselCaption = createElement(`
-      <div class='carpusel__caption'>
-        <span class='carousel__price'>${price}</span>
-        <div class='carousel__title'>${title}</div>
-        <button type="button" class="carousel__button">
-          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-        </button>
-      </div>
-      `);
-    return carouselCaption;
+  #carouselImg(url) {
+    return `<img src="/assets/images/carousel/${url}" class="carousel__img" alt="slide">`
   }
 
-  putCarouselCaption(carouselCaption){
-    this.elem.appendChild(carouselCaption);
+  #carouselCaption(price, title) {
+    return (
+      `
+        <div class="carousel__caption">
+          <span class="carousel__price">€${price}</span>
+          <div class="carousel__title">${title}</div>
+          ${this.#carouselButton()};
+        </div>
+      `
+    )
+  }
+
+  #carouselButton() {
+    const button = document.createElement('button');
+    button.classList = 'carousel__button';
+    button.type = 'button';
+
+    button.addEventListener('click', () => {
+      let event = new CustomEvent('product-add', {
+        bubbles: true,
+        detail: product.id
+      });
+
+      button.dispatchEvent(event)
+    });
+
+    return button.outerHTML;
   }
 }
